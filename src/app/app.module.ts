a22de3b30d3aa2240196e91eaed8a3abead5eff7
annapurnapatil <annapurnapatil23@gmail.com>
2020-10-04 13:01:33 +0200

    moving to public github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import {AutofocusModule} from 'angular-autofocus-fix';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { FormsModule } from '@angular/forms';
+import { TodoService } from './todo.service';
+
+const routes: Routes = [
+	{path: '', component: TodoListComponent, pathMatch: 'full'},
+	{path: ':filter', component: TodoListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // AutofocusModule,
+		RouterModule.forRoot(routes, {useHash: true})
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
